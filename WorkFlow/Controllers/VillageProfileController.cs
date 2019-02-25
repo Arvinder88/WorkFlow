c3417cea1732cc2d5a8f354d8996ab6adc88b200
Arvinder88 <Arvi.dhaliwal@gmail.com>
2019-02-25 12:20:24 +0530

    firstcommit

---
+++ WorkFlow/Controllers/VillageProfileController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WorkFlow.Controllers
+{
+    public class VillageProfileController : Controller
+    {
+        // GET: VillageProfile
+        [HttpGet]
+        public ActionResult VillageProfile()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ WorkFlow/ViewModal/villageProfileVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WorkFlow.ViewModal
+{
+    public class villageProfileVM
+    {
+        public int ddlon { get; set; }
+     
+    }
+}
